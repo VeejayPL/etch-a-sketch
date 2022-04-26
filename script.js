8d4ca9fabe65127f3677070ff214017f3598f72e
VeejeyPL <wojciech.jurkowski4@gmail.com>
2022-04-26 15:43:04 +0200

    Add grid reset button

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ const container = document.querySelector("#container");
 const gridButton = document.querySelector("#gridButton");
 let square;
 
+// Reset button with default drawGrid
 gridButton.addEventListener("click", () => {
-  document.location.reload();
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    box.remove();
+  });
+  drawGrid(16);
 });
 
+// Draw the grid
 function drawGrid(gridSize) {
   container.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
   for (let i = 0; i < gridSize * gridSize; i++) {
