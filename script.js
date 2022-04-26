96eea1af889f0df0996088e268c76d343926c0ac
VeejeyPL <wojciech.jurkowski4@gmail.com>
2022-04-26 14:33:42 +0200

    Add hover and trail effect

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const container = document.querySelector("#container");
+const gridButton = document.querySelector("#gridButton");
+let square;
+
+gridButton.addEventListener("click", () => {
+  document.location.reload();
+});
 
 function drawGrid(gridSize) {
   container.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
   for (let i = 0; i < gridSize * gridSize; i++) {
-    const square = container.appendChild(document.createElement("div"));
+    square = container.appendChild(document.createElement("div"));
     square.classList.add("box");
+    square.addEventListener("mouseover", (e) =>
+      e.target.classList.add("box-hover")
+    );
   }
 }
 
